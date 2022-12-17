497bd432098570bab8d9468d8c3028e1e4869f20
Cart <carthqviva2@gmail.com>
2022-12-17 19:37:19 +0530

    install axios

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 
 const Login = (props) => {
-  const host = "https://spkrapp.herokuapp.com";
-  const [credentials, setCredentials] = useState({ email: "", password: "" });
+  const host = 'https://spkrapp.herokuapp.com';
+  const [credentials, setCredentials] = useState({ email: '', password: '' });
   let history = useNavigate();
 
   const handleSubmit = async (e) => {
@@ -11,26 +11,29 @@ const Login = (props) => {
     const response = await fetch(`${host}/api/auth/login`, {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ email: credentials.email, password: credentials.password })
+      body: JSON.stringify({
+        email: credentials.email,
+        password: credentials.password,
+      }),
     });
     const json = await response.json();
     console.log(json);
     if (json.success) {
       // Save the auth and redirect
       localStorage.setItem('token', json.authToken);
-      localStorage.setItem('username',json.userName);
-      props.showAlert("Login Successfully","success");      
-      history("/");      
+      localStorage.setItem('username', json.userName);
+      props.showAlert('Login Successfully', 'success');
+      history('/');
     } else {
-      props.showAlert("Invalid credentials","danger");
+      props.showAlert('Invalid credentials', 'danger');
     }
-  }
+  };
 
   const onChange = (e) => {
-    setCredentials({ ...credentials, [e.target.name]: e.target.value })
-  }
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
 
   return (
     <div className="container">
@@ -38,19 +41,40 @@ const Login = (props) => {
         <h2>Sign in</h2>
         <form method="post" onSubmit={handleSubmit}>
           <div className="mb-3">
-            <label htmlFor="email" className="form-label">Email</label>
-            <input type="email" className="form-control" id="email" name="email" aria-describedby="emailHelp" value={credentials.email} onChange={onChange} />
+            <label htmlFor="email" className="form-label">
+              Email Address
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              aria-describedby="emailHelp"
+              value={credentials.email}
+              onChange={onChange}
+            />
             <div id="emailHelp" className="form-text"></div>
           </div>
           <div className="mb-3">
-            <label htmlFor="password" className="form-label">Password</label>
-            <input type="password" className="form-control" id="password" name="password" value={credentials.password} onChange={onChange} />
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={credentials.password}
+              onChange={onChange}
+            />
           </div>
-          <button type="submit" className="btn btn-primary">Login</button>
+          <button type="submit" className="btn btn-primary">
+            Login
+          </button>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
